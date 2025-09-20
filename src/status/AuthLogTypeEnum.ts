4d083948550c30d9d919eb0a51669dbd12b5248b
Minjae Lee <minj_shalom@mondrian.ai>
2025-09-20 23:10:50 +0900

    feat: Update entity

--- src/status/AuthLogTypeEnum.ts
+++ src/status/AuthLogTypeEnum.ts
@@ -1,7 +1,8 @@
 export enum AuthLogTypeEnum {
   SIGN_IN = "ALT001", // 로그인
   SIGN_OUT = "ALT002", // 로그아웃
-  SIGN_UP = "ALT003", // 계정 생성
-  UPDATE = "ALT004", // 계정 정보 수정
-  DELETE = "ALT005", // 계정 삭제
+  VERIFY_EMAIL = "ALT003", // 이메일 인증
+  SIGN_UP = "ALT004", // 계정 생성
+  UPDATE = "ALT005", // 계정 정보 수정
+  DELETE = "ALT006", // 계정 삭제
 }
